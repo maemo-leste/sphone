bde8fc046fcb9f97f8fb82f47fd1833ce085dd9a
uvos <devnull@uvos.xyz>
2021-11-12 12:08:22 +0100

    gui-sms: allow selection of backend, remove completion (for now)

--- src/gui/gui-sms.c
+++ src/gui/gui-sms.c
@@ -28,10 +28,8 @@
 #include "datapipe.h"
 #include "datapipes.h"
 #include "sphone-log.h"
-#include "store.h"
 #include "string.h"
 #include "gui-contact-view.h"
-#include "sphone-store-tree-model.h"
 #include "gui-sms.h"
 #include "gui-gtk-utils.h"
 
@@ -39,7 +37,7 @@ static void gui_sms_send_callback(GtkWidget *button, GtkWidget *main_window);
 static void gui_sms_cancel_callback(GtkWidget *button, GtkWidget *main_window);
 static void gui_sms_reply_callback(GtkWidget *button);
 
-static void gui_sms_coming_callback(gconstpointer data, gpointer user_data)
+static void gui_sms_incoming_callback(gconstpointer data, gpointer user_data)
 {
 	const MessageProperties *message = (const MessageProperties*)data;
 	(void)user_data;
@@ -55,63 +53,12 @@ static void gui_sms_open_contact_callback(GtkButton *button)
 
 void gtk_gui_sms_init(void)
 {
-	append_trigger_to_datapipe(&message_recived_pipe, gui_sms_coming_callback, NULL);
+	append_trigger_to_datapipe(&message_recived_pipe, gui_sms_incoming_callback, NULL);
 }
 
 void gtk_gui_sms_exit(void)
 {
-	remove_trigger_from_datapipe(&message_recived_pipe, gui_sms_coming_callback);
-}
-
-// The model will always contain only matched entries
-static gboolean gui_sms_completion_match(GtkEntryCompletion *completion, const gchar *key, GtkTreeIter *iter, gpointer user_data)
-{
-	(void)completion;
-	(void)key;
-	(void)iter;
-	(void)user_data;
-	return TRUE;
-}
-
-static gint gui_sms_to_changed_callback(GtkEntry *entry)
-{
-	const gchar *filter=gtk_entry_get_text(entry);
-	GtkEntryCompletion *completion=gtk_entry_get_completion(entry);
-
-	SphoneStoreTreeModel *dials_store;
-	if(*filter)
-		dials_store = sphone_store_tree_model_new(&SPHONE_STORE_TREE_MODEL_FILTER_MATCH_NAME_DIAL, filter);
-	else
-		dials_store = sphone_store_tree_model_new(NULL, NULL);
-		
-	g_object_set(G_OBJECT(dials_store), "max-rows", 25,NULL);
-	gtk_entry_completion_set_model(completion, GTK_TREE_MODEL(dials_store));
-	return FALSE;
-}
-
-static void gui_sms_to_changed_callback_delayed(GtkEntry *entry)
-{
-	static int timeout_source_id=-1;
-	if(timeout_source_id>-1)
-		g_source_remove(timeout_source_id);
-	timeout_source_id=g_timeout_add_seconds(1,(GSourceFunc)gui_sms_to_changed_callback, entry);
-}
-
-static void gui_sms_completion_cell_data(GtkCellLayout *cell_layout, GtkCellRenderer *cell,
-										 GtkTreeModel *tree_model, GtkTreeIter *iter, gpointer data)
-{
-	(void)cell_layout;
-	(void)data;
-
-	GValue dial_val={0},name_val={0};
-	
-	gtk_tree_model_get_value(tree_model, iter, SPHONE_STORE_TREE_MODEL_COLUMN_DIAL,&dial_val);
-	gtk_tree_model_get_value(tree_model, iter, SPHONE_STORE_TREE_MODEL_COLUMN_DIAL,&name_val);
-	gchar *content;
-	content=g_strdup_printf("%s <%s>", g_value_get_string(&name_val), g_value_get_string(&dial_val));
-	g_object_set_data_full(G_OBJECT(cell), "text", content,g_free);
-	g_value_unset(&dial_val);
-	g_value_unset(&name_val);
+	remove_trigger_from_datapipe(&message_recived_pipe, gui_sms_incoming_callback);
 }
 
 #ifdef ENABLE_LIBHILDON
@@ -162,9 +109,21 @@ bool gtk_gui_sms_send_show(const MessageProperties *msg)
 	GtkWidget *s = gtk_scrolled_window_new (NULL, NULL);
 	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (s),
 		       GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+	GtkWidget *backend_combo;
+	GtkWidget *selector = NULL;
+	(void)selector;
 	
 #ifdef ENABLE_LIBHILDON
+	selector = gui_sms_create_selector();
+	backend_combo = hildon_picker_button_new(HILDON_SIZE_AUTO, HILDON_BUTTON_ARRANGEMENT_HORIZONTAL);
+	hildon_button_set_title (HILDON_BUTTON(backend_combo), "Backend");
+	hildon_picker_button_set_selector(HILDON_PICKER_BUTTON(backend_combo),
+	                                 HILDON_TOUCH_SELECTOR(selector));
 	hildon_gtk_window_set_portrait_flags(GTK_WINDOW(main_window), HILDON_PORTRAIT_MODE_SUPPORT);
+	g_object_set_data(G_OBJECT(main_window), "backend-combo-box", selector);
+#else
+	backend_combo = gui_sms_create_backend_combo();
+	g_object_set_data(G_OBJECT(main_window), "backend-combo-box", backend_combo);
 #endif
 
 	GtkTextBuffer *text_buffer=gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_edit));
@@ -173,37 +132,13 @@ bool gtk_gui_sms_send_show(const MessageProperties *msg)
 		gtk_entry_set_text(GTK_ENTRY(to_entry), msg->line_identifier);
 	if(msg && msg->text)
 		gtk_text_buffer_set_text(text_buffer, msg->text, -1);
-
-	GtkCellRenderer *renderer;
-	GtkEntryCompletion *completion=gtk_entry_completion_new();
-	gtk_entry_completion_set_match_func(completion,gui_sms_completion_match,NULL,NULL);
-	gtk_entry_completion_set_popup_completion(completion,TRUE);
-	g_object_set(G_OBJECT(completion), "text-column", SPHONE_STORE_TREE_MODEL_COLUMN_DIAL,NULL);
-
-	renderer = gtk_cell_renderer_pixbuf_new();
-	gtk_cell_renderer_set_fixed_size(renderer,40,40);
-	gtk_cell_layout_pack_start (GTK_CELL_LAYOUT (completion),renderer, TRUE);
-	gtk_cell_layout_add_attribute(GTK_CELL_LAYOUT(completion),renderer,"pixbuf", SPHONE_STORE_TREE_MODEL_COLUMN_PICTURE);
-
-	renderer = gtk_cell_renderer_text_new();
-	gtk_cell_renderer_set_fixed_size(renderer,-1,40);
-	g_object_set(G_OBJECT(renderer), "ellipsize", PANGO_ELLIPSIZE_END, NULL);
-	gtk_cell_layout_pack_start (GTK_CELL_LAYOUT (completion),renderer, TRUE);
-	gtk_cell_layout_add_attribute(GTK_CELL_LAYOUT(completion),renderer,"text", SPHONE_STORE_TREE_MODEL_COLUMN_NAME);
-
-	renderer = gtk_cell_renderer_text_new();
-	gtk_cell_renderer_set_fixed_size(renderer,-1,40);
-	g_object_set(G_OBJECT(renderer), "ellipsize", PANGO_ELLIPSIZE_END, NULL);
-	gtk_cell_layout_pack_start (GTK_CELL_LAYOUT (completion),renderer, TRUE);
-	gtk_cell_layout_add_attribute(GTK_CELL_LAYOUT(completion),renderer,"text", SPHONE_STORE_TREE_MODEL_COLUMN_DIAL);
-	
-	gtk_entry_set_completion (GTK_ENTRY(to_entry),completion);
 	
 	gtk_text_view_set_border_window_size(GTK_TEXT_VIEW(text_edit),GTK_TEXT_WINDOW_LEFT,2);
 	gtk_text_view_set_border_window_size(GTK_TEXT_VIEW(text_edit),GTK_TEXT_WINDOW_RIGHT,2);
 
 	gtk_box_pack_start(GTK_BOX(to_bar), to_label, FALSE, FALSE, 0);
 	gtk_box_pack_start(GTK_BOX(to_bar), to_entry, TRUE, TRUE, 0);
+	gtk_box_pack_start(GTK_BOX(to_bar), backend_combo, TRUE, TRUE, 0);
 	gtk_container_add(GTK_CONTAINER(actions_bar), send_button);
 	gtk_container_add(GTK_CONTAINER(actions_bar), cancel_button);
 	gtk_box_pack_start(GTK_BOX(v1), to_bar, FALSE, FALSE, 0);
@@ -217,10 +152,8 @@ bool gtk_gui_sms_send_show(const MessageProperties *msg)
 
 	gtk_widget_show_all(main_window);
 	
-	g_signal_connect(G_OBJECT(send_button),"clicked", G_CALLBACK(gui_sms_send_callback),main_window);
-	g_signal_connect(G_OBJECT(cancel_button),"clicked", G_CALLBACK(gui_sms_cancel_callback),main_window);
-	g_signal_connect(G_OBJECT(to_entry),"changed", G_CALLBACK(gui_sms_to_changed_callback_delayed),NULL);
-	gui_sms_to_changed_callback(GTK_ENTRY(to_entry));
+	g_signal_connect(G_OBJECT(send_button),"clicked", G_CALLBACK(gui_sms_send_callback), main_window);
+	g_signal_connect(G_OBJECT(cancel_button),"clicked", G_CALLBACK(gui_sms_cancel_callback), main_window);
 	return true;
 }
 
@@ -297,11 +230,20 @@ void gui_sms_send_callback(GtkWidget *button, GtkWidget *main_window)
 	gchar *text=NULL;
 	g_object_get(G_OBJECT(text_buffer),"text", &text, NULL);
 	
+	char *backend_name = NULL;
+	GtkWidget *backend_combo = g_object_get_data(G_OBJECT(main_window), "backend-combo-box");
+	
+#ifdef ENABLE_LIBHILDON
+	backend_name = hildon_touch_selector_get_current_text(HILDON_TOUCH_SELECTOR(backend_combo));
+#else
+	backend_name = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(backend_combo));
+#endif
+
 	if(strlen(text) > 0 && strlen(to) > 0) {
 		MessageProperties *message = g_malloc0(sizeof(*message));
 		message->line_identifier = g_strdup(to);
 		message->text = text;
-		message->backend = sphone_comm_default_backend()->id;
+		message->backend = sphone_comm_find_backend_id(backend_name);
 		message->time = time(NULL);
 		execute_datapipe(&message_send_pipe, message);
 		message_properties_free(message);
