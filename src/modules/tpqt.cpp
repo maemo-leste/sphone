6f1c5630cf218456d5bd8255ef80c37c4ee1e4c2
Merlijn Wajer <merlijn@wizzup.org>
2023-01-21 20:43:12 +0100

    superwip

--- src/modules/tpqt.cpp
+++ src/modules/tpqt.cpp
@@ -1,12 +1,14 @@
 #include <glib.h>
 
-extern "C" 
+extern "C"
 {
 
+#include "types.h"
 #include "sphone-modules.h"
 #include "sphone-log.h"
 #include "datapipe.h"
 #include "datapipes.h"
+#include "comm.h"
 
 }
 
@@ -17,172 +19,149 @@ extern "C"
 #include <QtCore>
 #include <QtDBus/QtDBus>
 
+#include <voicecallmanager.h>
 
-#include <TelepathyQt/AbstractClientHandler>
-#include <TelepathyQt/AccountManager>
-#include <TelepathyQt/CallChannel>
-#include <TelepathyQt/ClientRegistrar>
-#include <TelepathyQt/PendingOperation>
-#include <TelepathyQt/PendingReady>
-#include <TelepathyQt/ChannelClassSpec>
-#include <TelepathyQt/AccountSet>
+struct tp_if_priv_s {
+	int backend_id;
+};
 
-class Telepathy : public QObject {
-Q_OBJECT
+class MaemoCallManager : public QObject
+{
+    Q_OBJECT
 
 public:
-    explicit Telepathy(QObject *parent = nullptr);
-    ~Telepathy() override;
+    MaemoCallManager();
+    void setup(void);
 
-    //QList<TelepathyAccount*> accounts;
-
-signals:
-    //void databaseAddition(ChatMessage *msg);
-    void accountManagerReady();
+    void acceptTrigger(void);
+    void rejectTrigger(void);
+    void dialTrigger(void);
 
 public slots:
-    //void sendMessage(const QString &local_uid, const QString &remote_uid, const QString &message);
-    //v/oid onDatabaseAddition(ChatMessage *msg);
+    void providersChanged();
+    void voiceCallsChanged();
+    void defaultProviderChanged();
+    void activeVoiceCallChanged();
 
-private slots:
-    void onAccountManagerReady(Tp::PendingOperation *op);
+    void audioModeChanged();
+    void audioRoutedChanged();
+    void microphoneMutedChanged();
+    void speakerMutedChanged();
+
+
+    void voiceCallStatusChanged();
+
+    /*
+     * TODO: we can hook these up just for testing:
+    */
+
+signals:
 
 private:
-    Tp::ClientRegistrarPtr registrar;
-    Tp::AccountManagerPtr m_accountmanager;
-    Tp::AbstractClientPtr clienthandler;
+
+public:
+    struct tp_if_priv_s *priv;
+    VoiceCallManager *mgr;
+
 };
 
-class TelepathyHandler : public Tp::AbstractClientHandler {
-public:
-    TelepathyHandler(const Tp::ChannelClassSpecList &channelFilter);
-    ~TelepathyHandler() { }
-    bool bypassApproval() const;
-    void handleChannels(const Tp::MethodInvocationContextPtr<> &context,
-        const Tp::AccountPtr &account,
-        const Tp::ConnectionPtr &connection,
-        const QList<Tp::ChannelPtr> &channels,
-        const QList<Tp::ChannelRequestPtr> &requestsSatisfied,
-        const QDateTime &userActionTime,
-        const Tp::AbstractClientHandler::HandlerInfo &handlerInfo);
-
-    void setTelepathyParent(Telepathy* parent);
-public:
-    Telepathy* m_telepathy_parent;
-
-};
+static MaemoCallManager* maemo_mgr;
 
 #include "tpqt.moc"
 
-Telepathy::Telepathy(QObject *parent) : QObject(parent) {
-    Tp::AccountFactoryPtr accountFactory = Tp::AccountFactory::create(
-                QDBusConnection::sessionBus(),
-                Tp::Features()
-                    << Tp::Account::FeatureCore
-                );
-
-    Tp::ConnectionFactoryPtr connectionFactory = Tp::ConnectionFactory::create(
-                QDBusConnection::sessionBus(),
-                Tp::Features()
-                    << Tp::Connection::FeatureCore
-                    << Tp::Connection::FeatureSelfContact
-                );
-
-    Tp::ChannelFactoryPtr channelFactory = Tp::ChannelFactory::create(QDBusConnection::sessionBus());
-
-    channelFactory->addCommonFeatures(Tp::Channel::FeatureCore);
-
-    channelFactory->addFeaturesForCalls(
-            Tp::Features()
-            << Tp::CallChannel::FeatureCallState
-            << Tp::CallChannel::FeatureCallMembers
-            << Tp::CallChannel::FeatureContents
-            << Tp::CallChannel::FeatureLocalHoldState
-            );
-
-    Tp::ContactFactoryPtr contactFactory = Tp::ContactFactory::create(
-                Tp::Features()
-                    << Tp::Contact::FeatureAlias
-                    << Tp::Contact::FeatureAvatarData
-                );
-
-    m_accountmanager = Tp::AccountManager::create(accountFactory);
-    connect(m_accountmanager->becomeReady(), &Tp::PendingReady::finished, this, &Telepathy::onAccountManagerReady);
-
-    registrar = Tp::ClientRegistrar::create(accountFactory,
-                                            connectionFactory,
-                                            channelFactory,
-                                            contactFactory);
-
-    auto tphandler = new TelepathyHandler(Tp::ChannelClassSpecList() << Tp::ChannelClassSpec::audioCall()
-	<< Tp::ChannelClassSpec::videoCall()
-	<< Tp::ChannelClassSpec::videoCallWithAudio()
-	<< Tp::ChannelClassSpec::streamedMediaCall()
-	<< Tp::ChannelClassSpec::streamedMediaAudioCall()
-	<< Tp::ChannelClassSpec::streamedMediaVideoCall()
-	<< Tp::ChannelClassSpec::streamedMediaVideoCallWithAudio());
-
-    Tp::AbstractClientPtr handler = Tp::AbstractClientPtr::dynamicCast(
-            Tp::SharedPtr<TelepathyHandler>(tphandler));
-
-    tphandler->setTelepathyParent(this);
-
-    registrar->registerClient(handler, "sphone");
+MaemoCallManager::MaemoCallManager() {
 }
 
-Telepathy::~Telepathy()
-{
+void MaemoCallManager::setup(void) {
+    mgr = new VoiceCallManager(qApp);
+
+    connect(mgr, &VoiceCallManager::providersChanged, this, &MaemoCallManager::providersChanged);
+    connect(mgr, &VoiceCallManager::voiceCallsChanged, this, &MaemoCallManager::voiceCallsChanged);
+    connect(mgr, &VoiceCallManager::defaultProviderChanged, this, &MaemoCallManager::defaultProviderChanged);
+    connect(mgr, &VoiceCallManager::activeVoiceCallChanged, this, &MaemoCallManager::activeVoiceCallChanged);
+
+    connect(mgr, &VoiceCallManager::audioModeChanged, this, &MaemoCallManager::audioModeChanged);
+    connect(mgr, &VoiceCallManager::audioRoutedChanged, this, &MaemoCallManager::audioRoutedChanged);
+    connect(mgr, &VoiceCallManager::microphoneMutedChanged, this, &MaemoCallManager::microphoneMutedChanged);
+    connect(mgr, &VoiceCallManager::speakerMutedChanged, this, &MaemoCallManager::speakerMutedChanged);
+
+    qDebug() << "default Provider" << mgr->defaultProviderId();
 }
 
-void Telepathy::onAccountManagerReady(Tp::PendingOperation *op) {
-    qDebug() << "onAccountManagerReady";
+void MaemoCallManager::providersChanged(void) {
+    qDebug() << "voiceCallsChanged";
+}
 
-    auto validaccounts = m_accountmanager->validAccounts();
-    auto l = validaccounts->accounts();
+void MaemoCallManager::voiceCallsChanged(void) {
+    qDebug() << "voiceCallsChanged";
+}
 
-    qDebug() << "account count:" << l.count();
+void MaemoCallManager::defaultProviderChanged(void) {
+    qDebug() << "defaultProviderChanged";
+}
 
-    Tp::AccountPtr acc;
+void MaemoCallManager::activeVoiceCallChanged(void) {
+    qDebug() << "activeVoiceCallChanged";
 
-    for (int i = 0; i < l.count(); i++) {
-        acc = l[i];
-        //auto myacc = new TelepathyAccount(acc);
-        //accounts << myacc;
+    VoiceCallHandler *handler = mgr->activeVoiceCall();
 
-        //connect(myacc, SIGNAL(databaseAddition(ChatMessage *)), SLOT(onDatabaseAddition(ChatMessage *)));
+    if (handler) {
+        connect(handler, &VoiceCallHandler::statusChanged, this, &MaemoCallManager::voiceCallStatusChanged);
+
+        CallProperties *call = (CallProperties*)g_malloc0(sizeof(*call));
+        call->backend = priv->backend_id;
+        call->needs_route = false;
+        call->outbound = false;
+        // TODO:
+        // call->line_identifier
+        // call->state
+        // call->emergency
+        // call->backend_data
+        execute_datapipe(&call_new_pipe, call);
     }
-
-    emit accountManagerReady();
 }
 
-
-TelepathyHandler::TelepathyHandler(const Tp::ChannelClassSpecList &channelFilter)
-    : Tp::AbstractClientHandler(channelFilter) {
-    // XXX: Do we want to do anything here?
-
+void MaemoCallManager::audioModeChanged(void) {
+    qDebug() << "audioModeChanged" << mgr->audioMode();
 }
 
-void TelepathyHandler::setTelepathyParent(Telepathy* parent) {
-    m_telepathy_parent = parent;
-    return;
+void MaemoCallManager::audioRoutedChanged(void) {
+    qDebug() << "audioRoutedChanged" << mgr->isAudioRouted();
 }
 
-bool TelepathyHandler::bypassApproval() const {
-    return false;
+void MaemoCallManager::microphoneMutedChanged(void) {
+    qDebug() << "microphoneMutedChanged" << mgr->isMicrophoneMuted();
 }
 
-void TelepathyHandler::handleChannels(const Tp::MethodInvocationContextPtr<> &context,
-                                      const Tp::AccountPtr &account,
-                                      const Tp::ConnectionPtr &connection,
-                                      const QList<Tp::ChannelPtr> &channels,
-                                      const QList<Tp::ChannelRequestPtr> &requestsSatisfied,
-                                      const QDateTime &userActionTime,
-                                      const Tp::AbstractClientHandler::HandlerInfo &handlerInfo) {
-    context->setFinished();
+void MaemoCallManager::speakerMutedChanged(void) {
+    qDebug() << "speakerMutedChanged" << mgr->isSpeakerMuted();
 }
 
+void MaemoCallManager::voiceCallStatusChanged(void) {
+    VoiceCallHandler *handler = mgr->activeVoiceCall();
+    qDebug() << "voiceCallStatusChanged: status" << handler->statusText();
+}
 
-static Telepathy *telepathy;
+static void call_hangup_trigger(gconstpointer data, gpointer user_data) {
+    // TODO: just use maemo_mgr->call_hangup_trigger ?
+    fprintf(stderr, "hangup\n");
+    VoiceCallHandler *handler = maemo_mgr->mgr->activeVoiceCall();
+    handler->hangup();
+}
+
+static void call_accept_trigger(gconstpointer data, gpointer user_data) {
+    // TODO: just use maemo_mgr->call_accept_trigger
+    fprintf(stderr, "accept\n");
+    VoiceCallHandler *handler = maemo_mgr->mgr->activeVoiceCall();
+    handler->answer();
+}
+
+static void call_dial_trigger(gconstpointer data, gpointer user_data) {
+    // TODO: just use maemo_mgr->call_dial_trigger
+	const CallProperties *call = (const CallProperties*)data;
+    fprintf(stderr, "dial: %s\n", call->line_identifier);
+    maemo_mgr->mgr->dial(call->line_identifier);
+}
 
 /** Functionality provided by this module */
 static const gchar *const provides[] = { MODULE_NAME, NULL };
@@ -197,32 +176,54 @@ G_MODULE_EXPORT module_info_struct module_info = {
 	.priority = 250
 };
 
+static const Scheme call_scheme =
+{
+	.scheme = (char*)"tel",
+	.flags = BACKEND_FLAG_CALL
+};
+
 extern "C" 
 {
 
-#if 0
-void contact_show_trigger(const void* data, void* user_data)
-{
-	(void)data;
-	sphone_module_log(LL_DEBUG, "%s", __func__);
-}
-#endif
 
-G_MODULE_EXPORT const gchar *sphone_module_init(void);
-const gchar *sphone_module_init(void)
+G_MODULE_EXPORT const gchar *sphone_module_init(void **data);
+const gchar *sphone_module_init(void **data)
 {
+	struct tp_if_priv_s *priv = (struct tp_if_priv_s*)g_malloc0(sizeof(*priv));
+	*data = priv;
 	sphone_module_log(LL_DEBUG, "%s", __func__);
 
-    telepathy = new Telepathy(NULL);
+    maemo_mgr = new MaemoCallManager();
+    maemo_mgr->setup();
+    maemo_mgr->priv = priv;
 
+	const Scheme* schemes[2] =
+	{
+		&call_scheme,
+		NULL
+	};
+
+	priv->backend_id = sphone_comm_add_backend("tptest", schemes, BACKEND_FLAG_CALL);
+
+	append_trigger_to_datapipe(&call_dial_pipe,   call_dial_trigger, priv);
+	append_trigger_to_datapipe(&call_accept_pipe, call_accept_trigger, priv);
+	//append_trigger_to_datapipe(&call_hold_pipe,   call_hold_trigger, priv);
+	append_trigger_to_datapipe(&call_hangup_pipe, call_hangup_trigger, priv);
 
 	return NULL;
 }
 
-G_MODULE_EXPORT void g_module_unload(GModule *mod);
-void g_module_unload(GModule *mod)
+G_MODULE_EXPORT void sphone_module_exit(void* data);
+void sphone_module_exit(void* data)
 {
-	(void)mod;
+	struct tp_if_priv_s *priv = (struct tp_if_priv_s*)data;
+
+	sphone_comm_remove_backend(priv->backend_id);
+
+	remove_trigger_from_datapipe(&call_dial_pipe,   call_dial_trigger, priv);
+	remove_trigger_from_datapipe(&call_accept_pipe, call_accept_trigger, priv);
+	//remove_trigger_from_datapipe(&call_hold_pipe,   call_hold_trigger, priv);
+	remove_trigger_from_datapipe(&call_hangup_pipe, call_hangup_trigger,priv);
 }
 
 }
